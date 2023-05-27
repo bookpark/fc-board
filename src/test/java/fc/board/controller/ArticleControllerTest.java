fb7f649163bc92493607de104e26633ddf8ed920
Booki Park <bkbkgg@gmail.com>
2023-05-28 00:31:38 +0900

    #30 - 검색 기능을 위해 SearchType 옵션 전달, 테스트
    게시판 페이지의 검색바에서 검색 종류를 드롭 다운 메뉴로 제공할 때, 해당 값을 뷰가 알게 하지 않고 서버로부터 받아서 쓰게 만들기 위해 값을 전달하는 로직을 테스트와 함께 추가.

--- src/test/java/fc/board/controller/ArticleControllerTest.java
+++ src/test/java/fc/board/controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package fc.board.controller;
 
 import fc.board.config.SecurityConfig;
+import fc.board.domain.type.SearchType;
 import fc.board.dto.ArticleWithCommentsDto;
 import fc.board.dto.UserAccountDto;
 import fc.board.service.ArticleService;
@@ -61,6 +62,31 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 검색어와 함께 호출")
+    @Test
+    public void searchKeywordTest() throws Exception {
+        //given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class)))
+                .willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt()))
+                .willReturn(List.of(0, 1, 2, 3, 4));
+
+        //when & then
+        mvc.perform(get("/articles")
+                        .queryParam("searchType", searchType.name())
+                        .queryParam("searchValue", searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));
+        then(articleService).should().searchArticles(eq(searchType), eq(searchValue), any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
