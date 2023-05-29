d596894cdb8241056a559d383e89076153428565
Booki Park <bkbkgg@gmail.com>
2023-05-29 17:47:10 +0900

    #27 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성
    그 외 도메인 수정 부분은 마이너한 오타 수정임.

--- src/main/java/fc/board/dto/ArticleCommentDto.java
+++ src/main/java/fc/board/dto/ArticleCommentDto.java
@@ -2,6 +2,7 @@ package fc.board.dto;
 
 import fc.board.domain.Article;
 import fc.board.domain.ArticleComment;
+import fc.board.domain.UserAccount;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,11 @@ public record ArticleCommentDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
+    public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountdto, String content) {
+        return new ArticleCommentDto(null, articleId, userAccountdto, content,
+                null, null, null, null);
+    }
+
     public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountdto,
                                        String content, LocalDateTime createdAt, String createdBy,
                                        LocalDateTime modifiedAt, String modifiedBy) {
@@ -35,10 +41,10 @@ public record ArticleCommentDto(
         );
     }
 
-    public ArticleComment toEntity(Article entity) {
+    public ArticleComment toEntity(Article article, UserAccount userAccount) {
         return ArticleComment.of(
-                entity,
-                userAccountDto.toEntity(),
+                article,
+                userAccount,
                 content
         );
     }
